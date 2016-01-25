41c3b8461922ab5741c752f17cbdeb45ed35c936
wanghui <2396611405@qq.com>
2016-01-25 10:27:30 +0800

    Initial commit

---
+++ QiuBaiDemo/RootTableViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  RootTableViewCell.swift
+//  QiuBaiDemo
+//
+//  Created by wanghui on 16/1/25.
+//  Copyright © 2016年 王辉. All rights reserved.
+//
+
+import UIKit
+
+class RootTableViewCell: UITableViewCell {
+    
+    var userIcon :UIImageView! //用户头像
+    var userNickName:UILabel!//用户昵称
+    var userContent:UILabel! //用户发表内容
+    //当前屏幕宽度
+    let width = UIScreen.mainScreen().bounds.width
+    
+    override  init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier:reuseIdentifier)
+        
+        //加载子控件
+        
+        
+    }
+    //加载子控件
+    func leoaSubViews() {
+    //初始化用户头像
+        let iconFrame = CGRectMake(0, 0, 40, 40);
+        self.userIcon = UIImageView(frame: iconFrame)
+        self.userIcon.backgroundColor = UIColor.whiteColor()
+        self.userIcon.layer.cornerRadius = 20
+        self.userIcon.layer.masksToBounds = true
+        self.contentView.addSubview(self.userIcon)
+        //初始化用户昵称
+        let NickFrame = CGRectMake(CGRectGetMaxX(iconFrame), 0,width-CGRectGetMaxX(iconFrame) , 40)
+        self.userNickName = UILabel(frame: NickFrame)
+        self.userNickName.backgroundColor = UIColor.whiteColor()
+        self.contentView.addSubview(self.userNickName)
+        
+        //初始化用户发表内容
+        let contentFrame = CGRectMake(0, CGRectGetMaxY(iconFrame), UIScreen.mainScreen().bounds.width, 40)
+        self.userContent = UILabel(frame: contentFrame)
+        self.userContent.backgroundColor = UIColor.whiteColor()
+        self.contentView.addSubview(self.userContent)
+    }
+    
+//这个方法没有什么作用 出错的时候会调用这个方法
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+  override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
+
+
+
---
+++ QiuBaiDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QiuBaiDemo
+//
+//  Created by wanghui on 16/1/25.
+//  Copyright © 2016年 王辉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
